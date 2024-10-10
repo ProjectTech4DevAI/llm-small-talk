4386fc6d375b097560fc2d2237072611d3d1a0f0
Jerome White <jerome@thewex.net>
2024-10-10 23:59:34 +0530

    Include data file name in output

--- models/semantic-router/go.py
+++ models/semantic-router/go.py
@@ -1,6 +1,6 @@
 import sys
 import csv
-import logging
+from pathlib import Path
 from argparse import ArgumentParser
 from multiprocessing import Pool, Queue
 
@@ -9,8 +9,10 @@ from semantic_router import Route
 from semantic_router.layer import RouteLayer
 from semantic_router.encoders import OpenAIEncoder
 
-sagemaker_config_logger = logging.getLogger('sagemaker.config')
-sagemaker_config_logger.setLevel(logging.WARNING)
+from mylib import Logger
+
+# sagemaker_config_logger = logging.getLogger('sagemaker.config')
+# sagemaker_config_logger.setLevel(logging.WARNING)
 
 #
 #
@@ -30,20 +32,27 @@ class SemanticRouter:
     def __call__(self, query):
         return self.rl(query).name
 
+def extract(path, split):
+    return (pd
+            .read_csv(path)
+            .query(f'split == "{split}"'))
+
 #
 #
 #
-def func(incoming, outgoing, df):
-    router = SemanticRouter(df)
+def func(incoming, outgoing, dpath):
+    df = extract(dpath, 'train')
     static = {
+        'data': dpath.name,
         'train_n': len(df),
         'train_c': df['gt'].unique().size,
     }
+    router = SemanticRouter(df)
 
     while True:
         sample = incoming.get()
         query = sample['query']
-        logging.warning(query)
+        Logger.info(query)
         outgoing.put({
             **static,
             **sample,
@@ -55,28 +64,25 @@ def func(incoming, outgoing, df):
 #
 if __name__ == '__main__':
     arguments = ArgumentParser()
+    arguments.add_argument('--data', type=Path)
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
-
-    data = (pd
-            .read_csv(sys.stdin) # query,gt,split,seed
-            .groupby('split', sort=False))
 
     incoming = Queue()
     outgoing = Queue()
     initargs = (
         outgoing,
         incoming,
-        data.get_group('train'),
+        args.data,
     )
 
     with Pool(args.workers, func, initargs):
-        test = data.get_group('test')
-        for i in test.itertuples(index=False):
+        df = extract(args.data, 'test')
+        for i in df.itertuples(index=False):
             outgoing.put(i._asdict())
 
         writer = None
-        for _ in range(len(test)):
+        for _ in range(len(df)):
             row = incoming.get()
             if writer is None:
                 writer = csv.DictWriter(sys.stdout, fieldnames=row)
