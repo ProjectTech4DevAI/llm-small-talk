60aafacdf6579439b97adbe82330ffec90c02c06
Jerome White <jerome@thewex.net>
2024-10-18 17:37:45 +0530

    Add global label

--- analysis/plot-metrics.py
+++ analysis/plot-metrics.py
@@ -13,31 +13,41 @@ if __name__ == '__main__':
     arguments.add_argument('--output', type=Path)
     args = arguments.parse_args()
 
-    groups = (pd
-              .read_csv(sys.stdin)
-              .groupby('metric', sort=False))
+    df = pd.read_csv(sys.stdin)
+    models = df['model'].nunique()
+
+    groups = df.groupby('metric', sort=False)
     nrows = groups.ngroups
     (fig, axes) = plt.subplots(nrows=nrows, sharex=True)
 
     (width, height) = fig.get_size_inches()
     fig.set_size_inches(width, height * args.scale_height)
 
-    for (i, (ax, (m, df))) in enumerate(zip(axes, groups), 1):
-         sns.lineplot(
-             x='support',
-             y='score',
-             hue='metric',
-             style='metric',
-             data=df,
-             markers=True,
-             legend=False,
-             errorbar='pi',
-             err_style='bars',
-             ax=ax,
-         )
-         ax.set_xlabel('Training examples' if i == nrows else '')
-         ax.set_ylabel(m)
-         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:0.2f}'))
-         ax.grid(axis='both', alpha=0.4)
+    first = 1
+    for (i, (ax, (m, g))) in enumerate(zip(axes, groups), first):
+        legend = i == first
+        sns.lineplot(
+            x='train_n',
+            y='score',
+            hue='model',
+            style='model',
+            data=g,
+            markers=True,
+            legend=legend,
+            errorbar='pi',
+            # err_style='bars',
+            ax=ax,
+        )
+        ax.set_xlabel('Training examples' if i == nrows else '')
+        ax.set_ylabel(m)
+        ax.yaxis.set_major_formatter(StrMethodFormatter('{x:0.2f}'))
+        ax.grid(axis='both', alpha=0.4)
+
+        if legend:
+            ax.legend(
+                loc='upper center',
+                bbox_to_anchor=(0.5, 1.25),
+                ncols=models,
+            )
 
     plt.savefig(args.output, bbox_inches='tight')
